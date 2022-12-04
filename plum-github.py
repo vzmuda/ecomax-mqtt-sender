9dddff1fb2158fee4c1e2481e95c19ff395a538d
vzmuda <59027240+vzmuda@users.noreply.github.com>
2022-12-04 12:36:43 +0100

    Update plum-github.py

--- plum-github.py
+++ plum-github.py
@@ -2,7 +2,7 @@
 # Analizator EcoNet
 # (C) 2020 Tomasz Król https://github.com/twkrol/econetanalyze
 # Gwarancji żadnej nie daję. Ale można korzystać do woli i modyfikować wg potrzeb
-# you have to installed paho mqtt client  / pip install paho-mqtt
+# you have to installed paho mqtt client  / pip install paho-mqtt / pip3 install paho-mqtt
 
 import functools
 import math
